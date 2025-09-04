2a4310442a9012b2106f4056bd444ba51d4a1477
toniette <luis_ti@outlook.com>
2025-09-04 09:11:06 -0300

    add id to rank response

--- app/Presentation/Http/Response/RankSongsResponse.php
+++ app/Presentation/Http/Response/RankSongsResponse.php
@@ -7,6 +7,7 @@ use Spatie\LaravelData\Data;
 class RankSongsResponse extends Data
 {
     public function __construct(
+        public string $id,
         public string $title,
         public int $viewsCount,
         public string $thumbnailUrl,
